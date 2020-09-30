04ef4130b02a06862857f19c03734f27e3416ac4
otengabrokwah <62722267+otengabrokwah@users.noreply.github.com>
2020-09-30 18:45:26 +0000

    commented code deleted..

--- js/app.js
+++ js/app.js
@@ -55,21 +55,3 @@ function dataIterator(dataProfile) {
 }
 
 
-
-
-
-     //     document.getElementById('profileDisplay').innerHTML = `
-     //       <ul class="list-group">
-     //         <li class="list-group-item">Name: ${currentProfile.name}</li>
-     //         <li class="list-group-item">Age: ${currentProfile.age}</li>
-     //         <li class="list-group-item">Location: ${currentProfile.location}</li>
-     //         <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-     //       </ul>
-     //     `;
-
-     //     document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
-     //   } else {
-     //     // No more profiles
-     //     window.location.reload();
-     //   }
-     // }
